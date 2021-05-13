62757636fe57ff04f74abb2f2757e4337158f076
msimunovic2 <matea.simunovic@fina.hr>
2021-05-13 14:23:55 +0200

    init

---
+++ src/main/java/hr/msimunovic/moneyheist/MoneyHeistApplication.java
@@ -0,0 +1,13 @@
+package hr.msimunovic.moneyheist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyHeistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyHeistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/msimunovic/moneyheist/MoneyHeistApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.msimunovic.moneyheist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyHeistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
