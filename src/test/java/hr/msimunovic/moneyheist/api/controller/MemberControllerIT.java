3fabcab98c8a396a21d59b113cda02aed891aabf
matea <mateasimunovic01@gmail.com>
2021-07-29 12:34:21 +0200

    added integration tests for heist and member controllers

--- src/test/java/hr/msimunovic/moneyheist/api/controller/MemberControllerIT.java
+++ src/test/java/hr/msimunovic/moneyheist/api/controller/MemberControllerIT.java
@@ -1,52 +1,71 @@
-package hr.msimunovic.moneyheist.api.controller;
+package hr.msimunovic.moneyheist.IT;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import hr.msimunovic.moneyheist.common.enums.MemberSexEnum;
 import hr.msimunovic.moneyheist.common.enums.MemberStatusEnum;
-import hr.msimunovic.moneyheist.member.dto.MemberInfoDTO;
-import org.json.JSONException;
+import hr.msimunovic.moneyheist.member.dto.MemberDTO;
+import hr.msimunovic.moneyheist.skill.dto.SkillDTO;
+import hr.msimunovic.moneyheist.util.JsonUtil;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
-import java.util.List;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class MemberControllerIT {
 
     @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() throws JsonProcessingException, JSONException {
+    void getAllMembers_thenReturns200() throws Exception {
 
+        mockMvc.perform(get("/member/list")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(3))));
 
-        String response = this.restTemplate.getForObject("/member/list", String.class);
-        JSONAssert.assertEquals(createMembers(), response, false);
     }
 
-    private String createMembers() throws JsonProcessingException {
-        return objectMapper.writeValueAsString(Arrays.asList(
-                MemberInfoDTO.builder()
-                        .id(100L)
-                        .name("Tokyo")
-                        .status(MemberStatusEnum.AVAILABLE)
-                        .build(),
-                MemberInfoDTO.builder()
-                        .id(101L)
-                        .name("Berlin")
-                        .status(MemberStatusEnum.AVAILABLE)
-                        .build(),
-                MemberInfoDTO.builder()
-                        .id(102L)
-                        .name("Moscow")
-                        .status(MemberStatusEnum.INCARCERATED)
-                        .build()));
+    @Test
+    void saveMember_thenReturns201() throws Exception {
+
+        mockMvc.perform(post("/member")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.toJson(createMemberDTO())))
+                .andExpect(status().isCreated());
+    }
+
+    private MemberDTO createMemberDTO() {
+        return MemberDTO.builder()
+                .name("Nairobi")
+                .sex(MemberSexEnum.F)
+                .email("nairobi@ag04.com")
+                .skills(Arrays.asList(
+                        SkillDTO.builder()
+                                .name("driving")
+                                .level("****")
+                                .build(),
+                        SkillDTO.builder()
+                                .name("combat")
+                                .level("*")
+                                .build()))
+                .mainSkill("combat")
+                .status(MemberStatusEnum.AVAILABLE)
+                .build();
     }
 }
